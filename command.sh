d26cf0320606fcbb0c729b5f53e3b9fe98e30f3d
jzx <909712563@qq.com>
2025-04-17 09:43:34 +0800

    Update command.sh

--- command.sh
+++ command.sh
@@ -1,11 +1,11 @@
+# OBP for ER tasks : Some examples
 
-# OBP 
-# Beer
-python main.py --dataset_name Beer --batch_method ApproxAll --k0 1.9 --k1 0.13 --k2 300 --upper_ques 5 --model gpt-3.5-turbo-0125 --output_dir output/group_assignment > ./Results/Beer_gpt35-0125.log
+# # Beer
+python main.py --dataset_name Beer --batch_method ApproxAll --k0 1.9 --k1 0.13 --k2 300 --upper_ques 5 --model gpt-3.5-turbo-0125 --output_dir output/group_assignment > ./Results/Beer_gpt35-0125_1.9_0.13_300_5.log
 
-# # iTunes-Amazon
-python main.py --dataset_name iTunesAmazon --batch_method ApproxAll --k0 2.28 --k1 0.02 --k2 300 --upper_ques 20 --model gpt-3.5-turbo-0125 --output_dir output/group_assignment > ./Results/iTunes-Amazon_gpt35-0125.log
+python main.py --dataset_name Beer --batch_method ApproxAll --k0 1.9 --k1 0.13 --k2 300 --upper_ques 4 --model gpt-3.5-turbo-0125 --output_dir output/group_assignment > ./Results/Beer_gpt35-0125_1.9_0.13_300_4.log
 
-# Fodors-Zagats
-python main.py --dataset_name FodorsZagats --batch_method ApproxAll --k0 0.98 --k1 0.31 --k2 400 --upper_ques 5 --model gpt-3.5-turbo-0125 --output_dir output/group_assignment > ./Results/Fodors-Zagats_gpt35-0125.log
+# Walmart-Amazon
+python main.py --dataset_name WalmartAmazon --batch_method ApproxAll --k0 1.42 --k1 0.19 --k2 800 --upper_ques 5 --model gpt-3.5-turbo --output_dir output/group_assignment > ./Results/Walmart-Amazon_gpt35_1.42_0.19_800_5.log 
 
+python main.py --dataset_name WalmartAmazon --batch_method ApproxAll --k0 1.42 --k1 0.19 --k2 800 --upper_ques 4 --model gpt-3.5-turbo --output_dir output/group_assignment > ./Results/Walmart-Amazon_gpt35_1.42_0.19_800_4.log 
